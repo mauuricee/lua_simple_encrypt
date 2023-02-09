334603e80ec6fe014bef15230b076dc063f7badc
Maurice <118083037+mauuricee@users.noreply.github.com>
2023-02-09 21:20:30 +0100

    Update fonctions.lua
    Removed the "TableToString" function that was not mandatory as "table.concat" function can be used

--- fonctions.lua
+++ fonctions.lua
@@ -1,29 +1,10 @@
 math.randomseed(os.time()) -- Pour ne jamais avoir la même clé
 
-local byte, len, find, sub = string.byte, string.len, string.find, string.sub
+local byte, len, find, sub, concat = string.byte, string.len, string.find, string.sub, table.concat
 
-local Charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!!#$%&'()*+,-./:;<=>?`{|}~^_éèà@êâûöä"
+local Charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!!#$%&'()*+,-./:;<=>?`{|}~^_éàè@êâûöä"
 
 local Base = 98
-
-function TableToString(tbl) -- Permet de transformer une table en string (via GHUB)
-    local result = ""
-    for k, v in pairs(tbl) do
-        if type(k) == "string" then
-            result = result .. "" .. k
-        end
-
-        if type(v) == "table" then
-            result = result .. TableToString(v)
-        elseif type(v) == "boolean" then
-            result = result..tostring(v)
-        else
-            result = result..""..v..""
-        end
-        result = result .. ""
-    end
-    return result
-end
 
 function indexOf(t, val) -- Permet de repérér un mot dans une WordList pendant le décryptage
     for i, v in pairs(t) do
@@ -35,7 +16,7 @@ function indexOf(t, val) -- Permet de repérér un mot dans une WordList pendant
 end
 
 function ReturnBase(s)
-  if byte(s) == 32 then return 98 end -- Pour renvoyer le caractère d'espace
+  if byte(s) == 32 then return 98 end
   local baseID = find(Charset,s)
   if baseID ~= nil then
     return baseID
@@ -55,7 +36,7 @@ end
 
 
 function CheckBaseChar(s) -- Vérifie que les caractères écrits sont compris dans la base des caractères
-  local slen = string.len(s) -- Longueur de la phrase écrite
+  local slen = len(s) -- Longueur de la phrase écrite
   for i = 1,slen do -- Pour chaque caractère
     local char = string.sub(s,i,i) -- Isole le caractère
     local charnum = ReturnBase(char) -- Le convertit en chiffre selon la base
@@ -81,10 +62,10 @@ function KeyGen() -- Fonction générant une WordList
       chart = string.char(chart) -- Convertit le nombre décimal en caractère ASCII correspondant
       table.insert(word,chart) -- Insère dans le mot le caractère
     end
-    table.insert(wlist,TableToString(word)) -- Insère chaque mot généré dans la WordList
+    table.insert(wlist,concat(word)) -- Insère chaque mot généré dans la WordList
   end
   print("\n")
-  print(TableToString(wlist))
+  print(concat(wlist))
   return wlist
 end
 
@@ -106,15 +87,15 @@ function Encrypt(s,t) -- Fonction cryptant un texte alphanumérique avec une Wor
     return false -- Renvoie faux
   end
   local wlist = t
-  local snum = string.len(s) -- Longueur de la phrase
+  local snum = len(s) -- Longueur de la phrase
   local sres = {} -- Table qui contiendra la phrase cryptée
   for i = 1,snum do -- Boucle se répétant pour chaque caractère de la phrase
-    local sb = string.sub(s,i,i) -- Permet d'isoler chaque caractère dans la boucle
+    local sb = sub(s,i,i) -- Permet d'isoler chaque caractère dans la boucle
     sb = tonumber(ReturnBase(sb)) -- Permet de convertir le caractère en décimal selon la table ASCII
     sb = wlist[sb] -- Permet d'obtenir le mot de la WordList renseignée correspondant
     table.insert(sres,sb) -- Insère chaque mot de cryptage dans la phrase
   end
-  return TableToString(sres) -- Renvoie une phrase cryptée convertie en string
+  return concat(sres) -- Renvoie une phrase cryptée convertie en string
 end
 
 function Decrypt(s,t)
@@ -125,22 +106,22 @@ function Decrypt(s,t)
   local crypted = s
   local cryptedtable = {} -- Prépare la table pour isoler chaque mot de la phrase cryptée
   local uncrypted = {} -- Prépare la table de la phrase décryptée
-  local snum = tonumber(string.len(crypted))/4 -- Permet d'obtenir le nombre de caractères quand décrypté
+  local snum = tonumber(len(crypted))/4 -- Permet d'obtenir le nombre de caractères quand décrypté
   local wstart = 1
   local wend = 4 -- Longueur de chaque mot
   for i = 1,snum do -- Pour chaque mot de la phrase cryptée
-    local w = string.sub(crypted, wstart, wend) -- Permet d'isoler un mot de la phrase cryptée
+    local w = sub(crypted, wstart, wend) -- Permet d'isoler un mot de la phrase cryptée
     table.insert(cryptedtable,w) -- Insère le mot dans la table
     wstart = wstart+4 -- Passe au mot suivant
     wend = wend+4
   end
-  for k,v in pairs(cryptedtable) do -- Boucle permettant le décryptage
+  for i,v in pairs(cryptedtable) do -- Boucle permettant le décryptage
     if not indexOf(wlist,v) then -- Si contenu non trouvé
-      --MainMenu()
-      return "erreur zbi"
+      MainMenu()
+      return "La mauvaise clé semble avoir été renseignée."
     end
     local char = ReturnChar(indexOf(wlist,v)) -- Convertit selon la WordList
     table.insert(uncrypted,char) -- Ajoute le caractère déchiffré dans la table
   end
-  return TableToString(uncrypted) -- Renvoie la phrase décryptée
+  return concat(uncrypted) -- Renvoie la phrase décryptée
 end
