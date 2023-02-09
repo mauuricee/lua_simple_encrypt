49b4dd1f0741eaad5d5c4ef850209bb0300bed77
Maurice <118083037+mauuricee@users.noreply.github.com>
2023-02-09 01:21:01 +0100

    Update fonctions.lua
    Changed the method of cryption and characters finding method. Script is no more dependant to the string.char() and string.byte functions.

--- fonctions.lua
+++ fonctions.lua
@@ -1,5 +1,11 @@
 math.randomseed(os.time()) -- Pour ne jamais avoir la même clé
 
+local byte, len, find, sub = string.byte, string.len, string.find, string.sub
+
+local Charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!!#$%&'()*+,-./:;<=>?`{|}~^_éèà@êâûöä"
+
+local Base = 98
+
 function TableToString(tbl) -- Permet de transformer une table en string (via GHUB)
     local result = ""
     for k, v in pairs(tbl) do
@@ -28,22 +34,32 @@ function indexOf(t, val) -- Permet de repérér un mot dans une WordList pendant
     return false
 end
 
-function PrintTable(t) -- Permet d'imprimer tous les élements d'une table
-  if not (type(t) == "table") then
-    print("L'élément n'est pas une table")
-    return false
+function ReturnBase(s)
+  if byte(s) == 32 then return 98 end -- Pour renvoyer le caractère d'espace
+  local baseID = find(Charset,s)
+  if baseID ~= nil then
+    return baseID
   end
-  for k,v in pairs(t) do
-    print(v)
-  end
+  return false
 end
 
-function CheckASCIIChar(s) -- Vérifie que les caractères écrits sont compris dans la table ASCII
+function ReturnChar(n)
+  if n > 98 then
+    return false
+  end
+  if n == 98 then
+    return " "
+  end
+  return sub(Charset, n, n)
+end
+
+
+function CheckBaseChar(s) -- Vérifie que les caractères écrits sont compris dans la base des caractères
   local slen = string.len(s) -- Longueur de la phrase écrite
   for i = 1,slen do -- Pour chaque caractère
     local char = string.sub(s,i,i) -- Isole le caractère
-    local charnum = string.byte(char) -- Le convertit en décimale correspondant à la table ASCII
-    if charnum > 128 then -- Si le décimal obtenu n'est pas dans la table
+    local charnum = ReturnBase(char) -- Le convertit en chiffre selon la base
+    if charnum > Base then -- Si le décimal obtenu n'est pas dans la table
       return false -- Renvoie faux
     end
   end
@@ -52,7 +68,7 @@ end
 
 function KeyGen() -- Fonction générant une WordList
   local wlist = {}
-  for i = 1,128 do -- La Table ASCII comportant 128 bits
+  for i = 1,Base do -- La Table ASCII comportant 128 bits
     local word = {}
     for w = 1,4 do -- Chaque mot est composé de 4 caractères
       local chart = 0
@@ -76,7 +92,7 @@ function KeyImport(s)
   local wlist = {}
   local wstart = 1
   local wend = 4
-  for i = 1,128 do
+  for i = 1,Base do
     local word = string.sub(s,wstart,wend)
     table.insert(wlist,word)
     wstart = wstart+4
@@ -86,7 +102,7 @@ function KeyImport(s)
 end
 
 function Encrypt(s,t) -- Fonction cryptant un texte alphanumérique avec une WordList
-  if not CheckASCIIChar(s) then -- Si les caractères ne sont pas sur la table ASCII
+  if not CheckBaseChar(s) then -- Si les caractères ne sont pas sur la table ASCII
     return false -- Renvoie faux
   end
   local wlist = t
@@ -94,7 +110,7 @@ function Encrypt(s,t) -- Fonction cryptant un texte alphanumérique avec une Wor
   local sres = {} -- Table qui contiendra la phrase cryptée
   for i = 1,snum do -- Boucle se répétant pour chaque caractère de la phrase
     local sb = string.sub(s,i,i) -- Permet d'isoler chaque caractère dans la boucle
-    sb = tonumber(string.byte(sb)) -- Permet de convertir le caractère en décimal selon la table ASCII
+    sb = tonumber(ReturnBase(sb)) -- Permet de convertir le caractère en décimal selon la table ASCII
     sb = wlist[sb] -- Permet d'obtenir le mot de la WordList renseignée correspondant
     table.insert(sres,sb) -- Insère chaque mot de cryptage dans la phrase
   end
@@ -102,17 +118,14 @@ function Encrypt(s,t) -- Fonction cryptant un texte alphanumérique avec une Wor
 end
 
 function Decrypt(s,t)
-  if not CheckASCIIChar(s) then -- Si les caractères ne sont pas sur la table ASCII
+  if not CheckBaseChar(s) then -- Si les caractères ne sont pas sur la table ASCII
     return false -- Renvoie faux
   end
   local wlist = t
   local crypted = s
   local cryptedtable = {} -- Prépare la table pour isoler chaque mot de la phrase cryptée
   local uncrypted = {} -- Prépare la table de la phrase décryptée
-  local snum = (tonumber(string.len(crypted)))/4 -- Permet d'obtenir le nombre de caractères quand décrypté
-  if not (type(snum) == "number" and math.floor(snum) == snum) then -- Vérifie le cas précédent
-    return false
-  end
+  local snum = tonumber(string.len(crypted))/4 -- Permet d'obtenir le nombre de caractères quand décrypté
   local wstart = 1
   local wend = 4 -- Longueur de chaque mot
   for i = 1,snum do -- Pour chaque mot de la phrase cryptée
@@ -123,10 +136,10 @@ function Decrypt(s,t)
   end
   for k,v in pairs(cryptedtable) do -- Boucle permettant le décryptage
     if not indexOf(wlist,v) then -- Si contenu non trouvé
-      MainMenu()
-      return false
+      --MainMenu()
+      return "erreur zbi"
     end
-    local char = string.char(tonumber(indexOf(wlist,v))) -- Convertit selon la WordList
+    local char = ReturnChar(indexOf(wlist,v)) -- Convertit selon la WordList
     table.insert(uncrypted,char) -- Ajoute le caractère déchiffré dans la table
   end
   return TableToString(uncrypted) -- Renvoie la phrase décryptée
